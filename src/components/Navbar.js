5690ba7756e08fe8c60dd743fd803aa965ef6560
Sandree <78666373+sandro21-glitch@users.noreply.github.com>
2023-06-17 16:52:05 +0400

    Updated header width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
 
 
   return (
-    <header className={`fixed left-0 top-0 z-[999] overflow-hidden bg-navCl ${isNavOpen ? 'h-52' : 'h-14'} md:h-14  w-[98vw] text-center py-3 flex items-start transition-[height] ease-linear duration-300`}>
+    <header className={`fixed left-0 top-0 z-[999] overflow-hidden bg-navCl ${isNavOpen ? 'h-52' : 'h-14'} md:h-14  w-full text-center py-3 flex items-start transition-[height] ease-linear duration-300`}>
         <nav className='relative px-3 w-[98vw] max-w-maxWidth mx-auto flex items-center justify-between'>
             <h4 className="cursor-pointer text-lg md:text-xl lg:text-2xl" href="#"><code>&lt; sandro /&gt;</code></h4>
             <ul className='text-left space-y-2 md:space-y-0 md:space-x-5 space-x-0 md:flex absolute left-3 top-0 md:translate-y-0 translate-y-1/2 md:static'>
