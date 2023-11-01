9738e8f66bba53cdab93aad5f6c50821fc232194
Sandro <vipsandro11@gmail.com>
2023-11-01 16:25:02 +0400

    initial commit

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -19,24 +19,25 @@ const Tech = () => {
         <div className="grid grid-cols-1 gap-5 md:gap-0 md:grid-cols-2">
           <pre>
             <span className="text-highlightRed">let</span> languages = [<br />
-            <span className="text-highlightBlue"> 'HTML'</span>,<br />
-            <span className="text-highlightBlue"> 'CSS/SCSS'</span>,<br />
-            <span className="text-highlightBlue"> 'JavaScript'</span>]<br />
+            <span className="text-highlightBlue">   'HTML'</span>,<br />
+            <span className="text-highlightBlue">   'CSS/SCSS'</span>,<br />
+            <span className="text-highlightBlue">   'JavaScript'</span>]<br />
           </pre>
           <div>
             <pre>
               <span className="text-highlightRed">let</span> frameworks = [
               <br />
-              <span className="text-highlightBlue"> 'TailwindCSS'</span>,<br />
-              <span className="text-highlightBlue"> 'React'</span>]
+              <span className="text-highlightBlue">   'TailwindCSS'</span>,<br />
+              <span className="text-highlightBlue">   'React'</span>]
             </pre>
             <pre className="mt-5">
               <span className="text-highlightRed">let</span> others = [
               <br />
-              <span className="text-highlightBlue"> 'React Query'</span>,<br />
-              <span className="text-highlightBlue"> 'Redux Toolkit'</span>,
+              <span className="text-highlightBlue">   'React Query'</span>,<br />
+              <span className="text-highlightBlue">   'Redux Toolkit'</span>,<br />
+              <span className="text-highlightBlue">   'React Router'</span>,
               <br />
-              <span className="text-highlightBlue"> 'Supabase'</span>,<br />]
+              <span className="text-highlightBlue">   'Supabase'</span>,<br />]
             </pre>
           </div>
         </div>
