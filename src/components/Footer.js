78fd39c1ebb40b38906437e078b588c84f09f028
Sandro <vipsandro11@gmail.com>
2024-08-18 18:33:21 +0400

    update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <section className='w-[98vw] max-w-maxWidth mx-auto py-10 px-3'>
             <ul className='flex items-start flex-col md:flex-row md:space-y-0 md:space-x-5 md:justify-center space-y-5'>
                 <li className='p-2 bg-black  cursor-pointer hover:opacity-70 transition-opacity ease-in duration-200'>
-                   <a href="https://github.com/sandro21-glitch" className='flex items-center' target='_blank'>
+                   <a href="https://github.com/sandro21-glitch" className='flex items-center' target='_blank' rel="noreferrer">
                     <AiFillGithub className='text-white text-2xl' />
                     <p className='text-white ml-2 font-bold'>
                         GitHub
@@ -15,7 +15,7 @@ const Footer = () => {
                    </a>
                 </li>
                 <li className='p-2 bg-highlightBlue cursor-pointer hover:opacity-70 transition-opacity ease-in duration-200'>
-                    <a href="https://www.linkedin.com/in/sandro-19929a1b7/" className='flex items-center ' target='_blank'>
+                    <a href="https://www.linkedin.com/in/sandro-19929a1b7/" className='flex items-center' target='_blank' rel="noreferrer">
                     <AiFillLinkedin className='text-white text-2xl' />
                     <p className='text-white ml-2 font-bold'>
                     Linkedin
@@ -23,7 +23,7 @@ const Footer = () => {
                     </a>
                 </li>
                 <li className='p-2 bg-[#6ABECD] cursor-pointer hover:opacity-70 transition-opacity ease-in duration-200'>
-                  <a href="https://www.frontendmentor.io/profile/sandro21-glitch" className='flex items-center ' target='_blank'>
+                  <a href="https://www.frontendmentor.io/profile/sandro21-glitch" className='flex items-center ' target='_blank' rel="noreferrer">
                     <SiFrontendmentor className='text-white text-2xl' />
                     <p className='text-white ml-2 font-bold'>
                     FrontendMentor
